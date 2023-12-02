72ef61d18c8d31048d1601b98ca731bd00f7fa7f
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-12-01 17:53:59 -0700

    load shape data for testing

--- sketch.js
+++ sketch.js
@@ -6,23 +6,41 @@ let cols = height / cellSize;
 let shapes = [];
 let ui;
 
+function preload() {
+  shapeData = loadJSON('data/test-shapes.json');
+}
+
 function setup() {
   createCanvas(width, height + 200);
   textSize(16);
   fill(0);
 
   ui = new UI(); // setup buttons and input fields
-  ui.setupInputGrid(rows, cols); // setup input grid
+  // ui.setupInputGrid(rows, cols); // setup input grid  
+
+  loadShapeData();
 }
 
 function draw() {
   background(255);
   // display the input grid
-  ui.drawInputGrid();
+  // ui.drawInputGrid();
 
   noLoop();
 }
 
 function mousePressed() {
   ui.selectInputCell(mouseX, mouseY);
+}
+
+function loadShapeData() {
+  for (let key in shapeData) {
+    if (shapeData.hasOwnProperty(key)) {
+      let inputShape = shapeData[key];
+      // create shape
+      let newShape = new Shape(inputShape.title);
+      newShape.saveUserInput(inputShape.inputGrid);
+      shapes.push(newShape);
+    }
+  }
 }
