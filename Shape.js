408f1e8f950ee68542eb286841e582b8913f19ac
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-12-06 18:59:51 -0700

    improve boundaries

--- Shape.js
+++ Shape.js
@@ -17,7 +17,7 @@ class Shape {
     // to do: only save the shape, without the empty input space around it
     // - note: _inputGrid[0] is the top row, not bottom. flip it.
 
-    // create a new array to loop inputGrid
+    // create trim shape
     let trimShape = [];
     let leftIndex = _inputGrid[0].length - 1;
     let rightIndex = 0;
@@ -34,7 +34,7 @@ class Shape {
         }
       }
     }
-
+    // use leftIndex and rightIndex to trim the shape
     for (let i = 0; i < _inputGrid.length; i++) {
       if (_inputGrid[i].includes(true)) {
         let rowSlice = _inputGrid[i].slice(leftIndex, rightIndex + 1);
@@ -43,21 +43,46 @@ class Shape {
     }
 
     // create and save a shape that represents the boundary
+    // 4. structure code to regenerate the case layout
+
+    // grow the trim shape by increasing the width by 2
+    // create the boundary shape by expanding the trim shape horizontally
     let boundaryShape = [];
     for (let i = 0; i < trimShape.length; i++) {
       let newRow = [...trimShape[i]];
       newRow.unshift(false);
       newRow.push(false);
+      let firstTrue = newRow.indexOf(true);
+      let lastTrue = newRow.lastIndexOf(true);
+      this.setTrueBetween(newRow, firstTrue - 1, lastTrue + 1)
       boundaryShape.push(newRow);
     }
-    
-    for (let i = 0; i < boundaryShape.length; i++) {
-      let firstTrue = boundaryShape[i].indexOf(true);
-      let lastTrue = boundaryShape[i].lastIndexOf(true);
-      boundaryShape[i][firstTrue - 1] = true;
-      boundaryShape[i][lastTrue + 1] = true;
-    }
+    // add a row on the top of the trim shape that's the same as the row below it
     boundaryShape.push([...boundaryShape[boundaryShape.length - 1]]);
+
+    // add additional boundary cells for under hangs
+    for (let i = 0; i < boundaryShape.length; i++) {
+      for (let j = 0; j < boundaryShape[i].length; j++) {
+        if (boundaryShape[i][j] == false) {
+          // on rows that are not the top row, check if the cell above is true
+          if (i < (boundaryShape.length - 1) && boundaryShape[i + 1][j] == true) {
+            boundaryShape[i][j] = true;
+          }
+        }
+      }
+    }
+
+    // add additional boundary cells for over hangs
+    for (let i = boundaryShape.length - 1; i >= 0; i--) {
+      for (let j = 0; j < boundaryShape[i].length; j++) {
+        if (boundaryShape[i][j] == false) {
+          // on rows that are not the bottom row, check if the cell below is true
+          if (i > 0 && boundaryShape[i - 1][j] == true) {
+            boundaryShape[i][j] = true;
+          } 
+        }
+      }
+    }
     
     // save grids
     this.shape = trimShape;
@@ -69,5 +94,12 @@ class Shape {
     this.boundaryWidth = this.boundaryShape[0].length;
     this.shapeHeight = this.shape.length;
     this.shapeWidth = this.shape[0].length;
+  }
+
+  // Function to set values to true between two indices
+  setTrueBetween(array, startIndex, endIndex) {
+    for (let i = startIndex; i <= endIndex; i++) {
+      array[i] = true;
+    }
   }
 }
