02ffb1275e377258d94980cacb94a7e0ddcbba72
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-12-07 20:50:54 -0700

    store col on board

--- Board.js
+++ Board.js
@@ -1,11 +1,13 @@
 class Board {
     constructor() {
-        // [x, y]
+        // [y, x]
         this.startCoords = []; // left or bottom
         this.endCoords = []; // right or top
         this.len; // length of the board
         this.width; // width of the board
         this.thickness; // thickness of the board
         this.poi = [{}]; // points of interest, ex: joinery, labeling, etc.
+
+        this.col; // which column the board is in
     }
 }
