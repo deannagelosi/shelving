27d1baf995f71f6cdcfa5195e9e0051b27937855
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2024-03-25 10:49:44 -0600

    update white space for tabs

--- sketch.js
+++ sketch.js
@@ -6,68 +6,68 @@ let sa; // simulated annealing
 let initialScore;
 
 function preload() {
-  shapeData = loadJSON('data/sunny-shapes.json');
+    shapeData = loadJSON('data/sunny-shapes.json');
 }
 
 function setup() {
-  createCanvas(canvasWidth, canvasHeight);
-  textSize(16);
-  fill(0);
-  loadShapeData();
+    createCanvas(canvasWidth, canvasHeight);
+    textSize(16);
+    fill(0);
+    loadShapeData();
 
-  let shapesPos = []; // shape data plus positions
-  for (let i = 0; i < shapes.length; i++) {
-    let shapeData = {
-      data: shapes[i],
-      // pos is bottom left corner of the shape, including overhangs
-      posX: 0,
-      posY: 0
-    };
-    shapesPos.push(shapeData);
-  }
+    let shapesPos = []; // shape data plus positions
+    for (let i = 0; i < shapes.length; i++) {
+        let shapeData = {
+            data: shapes[i],
+            // pos is bottom left corner of the shape, including overhangs
+            posX: 0,
+            posY: 0
+        };
+        shapesPos.push(shapeData);
+    }
 
-  let initialSolution = new Solution(shapesPos);
-  initialSolution.setInitialSolution();
-  initialSolution.makeLayout();
-  initialSolution.calcScore();
-  initialScore = initialSolution.score;
-  console.log('Initial solution: ', initialScore);
+    let initialSolution = new Solution(shapesPos);
+    initialSolution.setInitialSolution();
+    initialSolution.makeLayout();
+    initialSolution.calcScore();
+    initialScore = initialSolution.score;
+    console.log('Initial solution: ', initialScore);
 
-  let tempMax = 1000;
-  let tempMin = 1;
+    let tempMax = 1000;
+    let tempMin = 1;
 
-  sa = new SimulatedAnnealing(
-    tempMax,
-    tempMin,
-    initialSolution
-  );
+    sa = new SimulatedAnnealing(
+        tempMax,
+        tempMin,
+        initialSolution
+    );
 }
 
 function draw() {
-  clear();
-  background(255);
+    clear();
+    background(255);
 
-  if (sa.epoch()) {
-    // continue optimization
-    // console.log(sa.currSolution.score);
-    sa.tempCurr = sa.coolingSchedule();
-  } else {
-    // optimization complete
-    sa.currSolution.showLayout();
-    console.log('Initial solution: ', initialScore, ', Final solution: ', sa.currSolution.score);
-    noLoop(); // stop draw loop
-  }
+    if (sa.epoch()) {
+        // continue optimization
+        // console.log(sa.currSolution.score);
+        sa.tempCurr = sa.coolingSchedule();
+    } else {
+        // optimization complete
+        sa.currSolution.showLayout();
+        console.log('Initial solution: ', initialScore, ', Final solution: ', sa.currSolution.score);
+        noLoop(); // stop draw loop
+    }
 }
 
 function loadShapeData() {
-  // loop preloaded data and populate shapes array
-  for (let key in shapeData) {
-    if (shapeData.hasOwnProperty(key)) {
-      let inputShape = shapeData[key];
-      // create shape
-      let newShape = new Shape(inputShape.title);
-      newShape.saveUserInput(inputShape.inputGrid, parseInt(inputShape.shapeDepth));
-      shapes.push(newShape);
+    // loop preloaded data and populate shapes array
+    for (let key in shapeData) {
+        if (shapeData.hasOwnProperty(key)) {
+            let inputShape = shapeData[key];
+            // create shape
+            let newShape = new Shape(inputShape.title);
+            newShape.saveUserInput(inputShape.inputGrid, parseInt(inputShape.shapeDepth));
+            shapes.push(newShape);
+        }
     }
-  }
 }
