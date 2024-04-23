7bffce5fc2bbe5d0d47586598c2be964d9ab54ee
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2024-04-23 15:21:09 -0600

    input new shapes

--- sketch.js
+++ sketch.js
@@ -6,86 +6,57 @@ let sa; // simulated annealing
 let initialScore;
 let loopCount;
 let newCase;
+let shapesPos;
 
-let useExample = false;
+let inputMode = true;
+let useExample = true;
 
 function preload() {
-    shapeData = loadJSON('data/sunny-shapes.json');
+    shapeData = loadJSON('data/test.json');
 }
 
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
     textSize(16);
     fill(0);
-    loadShapeData();
 
-    let shapesPos = []; // shape data plus positions
-    for (let i = 0; i < shapes.length; i++) {
-        let shapeData = {
-            data: shapes[i],
-            // pos is bottom left corner of the shape, including overhangs
-            posX: 0,
-            posY: 0,
-        };
-        shapesPos.push(shapeData);
+    if (inputMode == true) {
+        shapeInput = new ShapeInput(); // setup buttons and input fields
     }
-
-    let initialSolution = new Solution(shapesPos);
-    if (useExample) {
-        initialSolution.exampleSolution();
-    } else {
-        initialSolution.setInitialSolution();
-    }
-    initialSolution.makeLayout();
-    initialSolution.calcScore();
-    initialSolution.showLayout();
-    initialScore = initialSolution.score;
-
-    let tempMax = 5000;
-    let tempMin = 1;
-
-    sa = new SimulatedAnnealing(
-        tempMax,
-        tempMin,
-        initialSolution
-    );
-
-    loopCount = 0;
 }
 
 function draw() {
-    if (useExample) {
-        createCase();
-        noLoop(); // stop draw loop
+    if (inputMode) {
+        // display the input grid
+        shapeInput.drawInputGrid();
+        noLoop(); // don't loop input screen
 
     } else {
-        // run simulated annealing
-        if (sa.epoch()) {
-            // continue optimization
-            sa.tempCurr = sa.coolingSchedule();
-            loopCount++;
-            if (loopCount % 10 == 0) {
-                clear();
-                background(255);
-                sa.currSolution.showLayout()
-            }
-        } else {
-            // optimization complete
-            createCase();
-            noLoop(); // stop draw loop
+        if (!sa) {
+            initialSolution();
         }
-    }
-}
 
-function loadShapeData() {
-    // loop preloaded data and populate shapes array
-    for (let key in shapeData) {
-        if (shapeData.hasOwnProperty(key)) {
-            let inputShape = shapeData[key];
-            // create shape
-            let newShape = new Shape(inputShape.title);
-            newShape.saveUserInput(inputShape.inputGrid, parseInt(inputShape.shapeDepth));
-            shapes.push(newShape);
+        if (useExample) {
+            // use example solution on example shapes
+            createCase();
+            noLoop();
+        } 
+        else {
+            // optimization loop to anneal the solution
+            if (sa.epoch()) {
+                // continue optimization
+                sa.tempCurr = sa.coolingSchedule();
+                loopCount++;
+                if (loopCount % 10 == 0) {
+                    clear();
+                    background(255);
+                    sa.currSolution.showLayout()
+                }
+            } else {
+                // optimization complete
+                createCase();
+                noLoop(); // stop draw loop
+            }
         }
     }
 }
@@ -102,13 +73,63 @@ function createCase() {
     // show result
     sa.currSolution.showLayout();
     newCase.showResult();
-    
+
     console.log(sa.currSolution);
 }
 
 function keyPressed() {
-    if (key === 's' || key === 'S') {
-        // save current case as SVG
-        newCase.renderBoards();
+    if (!inputMode) {
+        if (key === 's' || key === 'S') {
+            // save current case as SVG
+            newCase.renderBoards();
+        }
     }
-  }
+}
+
+function mousePressed() {
+    if (inputMode) {
+        shapeInput.selectInputCell(mouseX, mouseY);
+    }
+}
+
+function mouseDragged() {
+    if (inputMode) {
+        shapeInput.selectInputCell(mouseX, mouseY);
+    }
+}
+
+function initialSolution() {
+    // populate the user input shapes into shapesPos
+    shapesPos = []; // shape data plus positions
+    for (let i = 0; i < shapes.length; i++) {
+        let shapeData = {
+            data: shapes[i],
+            // pos is bottom left corner of the shape, including overhangs
+            posX: 0,
+            posY: 0,
+        };
+        shapesPos.push(shapeData);
+    }
+
+    // generate the initial solution
+    let initialSolution = new Solution(shapesPos);
+    if (useExample) {
+        initialSolution.exampleSolution();
+    } else {
+        initialSolution.setInitialSolution();
+    }
+    initialSolution.makeLayout();
+    initialSolution.calcScore();
+    initialScore = initialSolution.score;
+
+    let tempMax = 5000;
+    let tempMin = 1;
+
+    sa = new SimulatedAnnealing(
+        tempMax,
+        tempMin,
+        initialSolution
+    );
+
+    loopCount = 0;
+}
