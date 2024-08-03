34b83064102488e884ed9967023d01f1c6b8d641
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2024-08-03 14:58:32 -0600

    simplify board class

--- js/Board.js
+++ js/Board.js
@@ -1,76 +1,56 @@
 class Board {
-    constructor(_startCoords, _endCoords, _orientation) {
-        // Make sure boards start and end are always left-right or bottom-top
-        // fix left-right orientation
-        this.orientation = _orientation; // horizontal or vertical
-        [this.startCoords, this.endCoords] = this.setBoardDirection(_startCoords, _endCoords, this.orientation);
-
-        this.merged = false; // if the board is merged with another board later
-
-        // this.depth; // depth of the board
-        // this.thickness; // thickness of the board
+    constructor(_firstCoord, _secondCoord, _orientation) {
+        // state variables
+        this.orientation = _orientation; // horizontal ('x') or vertical ('y')
+        this.coords = this.setBoardDirection(_firstCoord, _secondCoord, this.orientation);
+        this.len = this.getLength();
+        this.boardLabel;
 
         this.poi = { // points of interest
-            startJoint: "", // "pin" or "slot"
-            endJoint: "", // "pin" or "slot"
+            startJoint: this.orientation === "x" ? "pin" : "slot",
+            endJoint: this.orientation === "x" ? "pin" : "slot",
             tJoints: [], // array of x-values for T-joint holes 
             xJoints: [],
-            shapes: [{}], // array of objs with shape name and x-value position
         }
-        this.boardLabel;
+
+        // flags
+        this.merged = false; // if it has merged with another board
     }
 
     getLength() {
         // return the length of the board
         // check if the board is horizontal or vertical
-        if (this.startCoords.y == this.endCoords.y) {
+        if (this.coords.start.y == this.coords.end.y) {
             // same y, horizontal board
             // return x distance as length
-            return this.endCoords.x - this.startCoords.x;
+            return this.coords.end.x - this.coords.start.x;
         }
-        else if (this.startCoords.x == this.endCoords.x) {
+        else if (this.coords.start.x == this.coords.end.x) {
             // same x, vertical board
             // return y distance as length
-            return this.endCoords.y - this.startCoords.y;
+            return this.coords.end.y - this.coords.start.y;
         }
 
-        return null; // error
+        console.error("Board length error. start: ", this.coords.start, "end: ", this.coords.end);
+        return null;
     }
 
-    getCoords() {
-        // return the start and end coordinates
-        return [this.startCoords, this.endCoords];
-    }
-
-    setBoardDirection(_startCoords, _endCoords, _orientation){
+    setBoardDirection(_firstCoord, _secondCoord, _orientation) {
         // always build boards with the start side as the smaller x or y value
-
-        let startCoords;
-        let endCoords;
+        let startCoord;
+        let endCoord;
 
         if (this.orientation == "x") {
-            // fix left-right orientation
-            if (_startCoords.x < _endCoords.x) {
-                startCoords = _startCoords;
-                endCoords = _endCoords;
-            } else {
-                startCoords = _endCoords;
-                endCoords = _startCoords;
-            }
+            startCoord = _firstCoord.x < _secondCoord.x ? _firstCoord : _secondCoord;
+            endCoord = _firstCoord.x < _secondCoord.x ? _secondCoord : _firstCoord;
 
         } else if (this.orientation == "y") {
-            // fix bottom-top orientation
-            if (_startCoords.y < _endCoords.y) {
-                startCoords = _startCoords;
-                endCoords = _endCoords;
-            } else {
-                startCoords = _endCoords;
-                endCoords = _startCoords;
-            }
+            startCoord = _firstCoord.y < _secondCoord.y ? _firstCoord : _secondCoord;
+            endCoord = _firstCoord.y < _secondCoord.y ? _secondCoord : _firstCoord;
         } else {
-            throw new Error("Board missing orientation. start: ", _startCoords, "end: ", _endCoords, "orientation: ", _orientation);
+            console.error("Board missing orientation. start: ", _firstCoord, "end: ", _secondCoord, "orientation: ", _orientation);
         }
 
-        return [startCoords, endCoords];
+        return { start: startCoord, end: endCoord };
     }
 }
